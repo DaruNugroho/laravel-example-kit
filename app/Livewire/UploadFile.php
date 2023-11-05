efa087a1de3af1244b6c036d7953542f956acd16
Sandi Fadilah <fadilahsandi@gmail.com>
2023-11-05 21:33:47 +0700

    update ok

--- app/Livewire/UploadFile.php
+++ app/Livewire/UploadFile.php
@@ -2,12 +2,139 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Log;
+use Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class UploadFile extends Component
 {
+    use WithFileUploads;
+
+    public array $breadcrumb = [
+        ['lable' => 'Home', 'link' => '/livewire/home'],
+        ['lable' => 'Basic Crud', 'link' => '#']
+    ];
+
+    public $id = "";
+    public $name = "";
+    public $qty = "";
+    public $expiry_date = "";
+    #[Rule('image|max:1024')] // 1MB Max
+    public $photo;
+    #[Rule('image|max:1024')] // 1MB Max
+    public $photo_temp;
+
+    public function store()
+    {
+        $this->validate([
+            'name'          => 'required',
+            'qty'           => 'required',
+            'expiry_date'   => 'required|date',
+            'photo_temp'    => 'required|file|mimes:png,jpg,jpeg|max:1024',
+        ]);
+
+        // Store Photo
+        $image_name = time() . '-' . Str::slug($this->name) . '.' . $this->photo_temp->getClientOriginalExtension();
+        $image_link = $this->photo_temp->storeAs('uploads/images', $image_name, 'public');
+
+        Product::create([
+            'name' => $this->name,
+            'qty' => $this->qty,
+            'expiry_date' => $this->expiry_date,
+            'photo' => $image_link
+        ]);
+
+        $this->dispatch('notify', 'Created ');
+
+        $this->resetForm();
+    }
+
+    public function edit(string $id)
+    {
+        $this->resetForm();
+        $data = Product::find($id);
+
+        $this->id           = $data->id;
+        $this->name         = $data->name;
+        $this->qty          = $data->qty;
+        $this->expiry_date  = $data->expiry_date;
+        $this->photo        = $data->photo;
+    }
+
+    public function update(string $id)
+    {
+        $this->validate([
+            'name'          => 'required',
+            'qty'           => 'required',
+            'expiry_date'   => 'required|date',
+            'photo_temp'    => 'nullable|file|mimes:png,jpg,jpeg|max:1024',
+        ]);
+
+        $data = Product::find($id);
+
+        if ($data) {
+
+            if ($this->photo_temp) {
+
+                // Store Photo
+                $image_name = time() . '-' . Str::slug($this->name) . '.' . $this->photo_temp->getClientOriginalExtension();
+                $image_link = $this->photo_temp->storeAs('uploads/images', $image_name, 'public');
+
+                // Delete Photo
+                $this->deleteFile($data->photo);
+            } else {
+                $image_link = $data->photo;
+            }
+
+            $data->update([
+                'name' => $this->name,
+                'qty' => $this->qty,
+                'expiry_date' => $this->expiry_date,
+                'photo' => $image_link
+            ]);
+        }
+
+        $this->dispatch('notify', 'Updete Successfuly');
+
+        $this->resetForm();
+    }
+
+
+    public function delete(string $id)
+    {
+        $data = Product::find($id);
+        $this->deleteFile($data->photo);
+        $data->delete();
+        $this->resetForm();
+    }
+
+
+
+    public function deleteFile($file)
+    {
+        if (isset($file)) {
+            try {
+                $file_name  = explode("/", $file);
+                unlink(storage_path('app/public/uploads/images/') . $file_name[2]);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+        }
+    }
+
+
+    public function resetForm()
+    {
+        $this->resetValidation();
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.upload-file');
+        return view('livewire.upload-file', [
+            'data' => Product::paginate(12)
+        ]);
     }
 }
--- resources/views/livewire/upload-file.blade.php
+++ resources/views/livewire/upload-file.blade.php
@@ -1,66 +1,167 @@
 @push('pageTitle', 'Basic CRUD')
-@push('pageSubTitle', 'Example')
-@push('breadcrumb', ['Home', 'Layout'])
 
 <div class="content-wrapper">
 
-    <livewire:comps.title-page title="File Upload" />
+    <livewire:comps.title-page title="Basic CURD" subTitle="example" :breadcrumb="$breadcrumb" />
 
     <div class="content">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's
-                                content.
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                <div class="col-lg-5">
+
+
+                    <div class="card card-info">
+                        <div class="card-header">
+                            <h3 class="card-title">Form Livewire
+                            </h3>
                         </div>
+
+                        <form class="form-horizontal">
+                            <div class="card-body">
+
+                                <p class="card-text">You can use this form for create, update, and delete data. to clear
+                                    form click Reset Button. </p>
+
+
+
+                                <div class="form-group row">
+                                    <label for="photo" class="col-sm-4 col-form-label text-right">File
+                                        Product</label>
+                                    <div class="col-sm-8">
+                                        @if ($photo_temp)
+                                            <img src="{{ $photo_temp->temporaryUrl() }}" width="80"
+                                                style="padding:10px;">
+                                        @else
+                                            @if ($photo)
+                                                <img src="{{ asset('storage/' . $photo) }}" width="80"
+                                                    style="padding:10px;">
+                                            @endif
+                                        @endif
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" wire:model="photo_temp"
+                                                    class="custom-file-input @error('photo') is-invalid @enderror"
+                                                    id="photo">
+                                                <label class="custom-file-label" for="photo">Choose
+                                                    file</label>
+                                            </div>
+                                        </div>
+                                        @error('photo_temp')
+                                            <span class="error">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="name" class="col-sm-4 col-form-label text-right">Product
+                                        Name</label>
+                                    <div class="col-sm-8">
+                                        <input wire:model="name" type="text"
+                                            class="form-control @error('name') is-invalid @enderror" id="name"
+                                            placeholder="Product Name">
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="qty" class="col-sm-4 col-form-label text-right">Quantity</label>
+                                    <div class="col-sm-8">
+                                        <input wire:model="qty" type="number"
+                                            class="form-control @error('qty') is-invalid @enderror" id="qty"
+                                            placeholder="Qty">
+                                        @error('qty')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="expiry_date" class="col-sm-4 col-form-label text-right">Expiry
+                                        Date</label>
+                                    <div class="col-sm-8">
+                                        <input wire:model="expiry_date" type="date"
+                                            class="form-control @error('expiry_date') is-invalid @enderror"
+                                            id="expiry_date">
+                                        @error('expiry_date')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="card-footer">
+                                <button type="button" wire:click.prevent="{{ $id ? "update('$id')" : 'store' }}"
+                                    class="btn btn-info float-right">
+                                    @if ($id)
+                                        Update Data
+                                    @else
+                                        Save Data
+                                    @endif
+                                </button>
+                                @if ($id)
+                                    <a wire:click.prevent="delete('{{ $id }}')" wire:confirm="are u sure ?"
+                                        class="btn btn-danger ml-2">Delete</a>
+                                @endif
+                                <a wire:click.prevent="resetForm" class="btn btn-default">Reset</a>
+
+                            </div>
+
+                        </form>
                     </div>
-                    <div class="card card-primary card-outline">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's
-                                content.
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+
+                </div>
+                <div class="col-lg-7">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title m-0"> Data </h5>
+                        </div>
+                        <div class="card-body p-0">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">#</th>
+                                        <th colspan="2">Product</th>
+                                        <th>Qty</th>
+                                        <th>Expiry Date</th>
+                                        <th style="width: 40px">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $i => $item)
+                                        <tr>
+                                            <td>{{ $i + 1 }}</td>
+                                            <td style="width:40px;">
+                                                @if ($item->photo)
+                                                    <img class="direct-chat-img"
+                                                        src="{{ asset('storage/' . $item->photo) }}"
+                                                        class="img-circle elevation-2" alt="User Image">
+                                                @endif
+                                            </td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->qty }} </td>
+                                            <td>{{ $item->expiry_date }} </td>
+                                            <td>
+                                                <button class="btn btn-xs btn-outline-primary"
+                                                    wire:click.prevent="edit('{{ $item->id }}')"
+                                                    class="btn btn-sm btn-primary">Edit
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="card-title m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional
-                                content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
             </div>
 
         </div>
